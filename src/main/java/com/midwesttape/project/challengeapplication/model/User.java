cfcc73240f4a3f126090b90301a2876634b28f55
Drew Hite <drew@drewhite.org>
2023-01-18 22:15:31 -0500

    no need for @NoArgsConstructor

--- src/main/java/com/midwesttape/project/challengeapplication/model/User.java
+++ src/main/java/com/midwesttape/project/challengeapplication/model/User.java
@@ -8,7 +8,6 @@ import lombok.experimental.Accessors;
 @Data
 @Accessors(chain = true)
 @AllArgsConstructor
-@NoArgsConstructor
 public class User {
     private Long id;
     private String firstName;
